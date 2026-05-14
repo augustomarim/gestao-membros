e9ed22da707b0c92896afa64945cfb3502b6b45d
Augusto Luiz Marim <augustolm06@gmail.com>
2026-05-14 16:40:22 -0300

    feat: add Member entity

---
+++ backend/src/main/java/br/com/augustoluiz/backend/entity/Member.java
@@ -0,0 +1,34 @@
+package br.com.augustoluiz.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "members")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 11)
+    private String cpf;
+
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
+    @Column(nullable = false)
+    private Boolean active;
+}
