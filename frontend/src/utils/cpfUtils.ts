0102739c526f872c50dc9cb4bf257d37bbc2036d
Augusto Luiz Marim <augustolm06@gmail.com>
2026-05-14 16:42:32 -0300

    feat: add CPF utility functions

---
+++ frontend/src/utils/cpfUtils.ts
@@ -0,0 +1,11 @@
+export function formatCpf(value: string): string {
+  const digits = value.replace(/\D/g, '').slice(0, 11);
+  return digits
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+}
+
+export function cleanCpf(value: string): string {
+  return value.replace(/\D/g, '');
+}
